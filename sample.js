19a359ccfff3af7dac55961cf3e3ba56c326d114
makimac0203 <56378289+makimac0203@users.noreply.github.com>
2020-07-16 21:18:48 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,30 +1,26 @@
-const outputElement = document.getElementById('output_csv');
-
-function getCsvData(dataPath) {
- const request = new XMLHttpRequest();
- request.addEventListener('load', (event) => {
-  const response = event.target.responseText;
-  convertArray(response);
- });
- request.open('GET', dataPath, true);
- request.send();
+//CSVファイルを読み込む関数getCSV()の定義
+function getCSV(){
+    var req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
+    req.open("get", "sample.csv", true); // アクセスするファイルを指定
+    req.send(null); // HTTPリクエストの発行
+	
+    // レスポンスが返ってきたらconvertCSVtoArray()を呼ぶ	
+    req.onload = function(){
+	convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
+    }
 }
-
-function convertArray(data) {
- const dataArray = [];
- const dataString = data.split('\n');
- for (let i = 0; i < dataString.length; i++) {
-  dataArray[i] = dataString[i].split(',');
- }
- let insertElement = '';
- dataArray.forEach((element) => {
-  insertElement += '<tr>';
-  element.forEach((childElement) => {
-   insertElement += `<td>${childElement}</td>`
-  });
-  insertElement += '</tr>';
- });
- outputElement.innerHTML = insertElement;
+ 
+// 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
+function convertCSVtoArray(str){ // 読み込んだCSVデータが文字列として渡される
+    var result = []; // 最終的な二次元配列を入れるための配列
+    var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
+ 
+    // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
+    for(var i=0;i<tmp.length;++i){
+        result[i] = tmp[i].split(',');
+    }
+ 
+    alert(result[1][2]); // 300yen
 }
-
-getCsvData('./example.csv');
+ 
+getCSV(); //最初に実行される
