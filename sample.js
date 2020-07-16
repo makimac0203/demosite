286ee9e998c6c0b28ebae4a93c02afb5cdd50702
makimac0203 <56378289+makimac0203@users.noreply.github.com>
2020-07-16 20:41:53 +0900

    Update sample.js

--- sample.js
+++ sample.js
@@ -1,29 +1,13 @@
-// 正しいコード
-// <div id="r"></div>
-function getCSV() {
-  var req = new XMLHttpRequest();
-  req.open('get', 'sample.csv', true);
-  req.send(null);
-  req.onload = function() {
-    setCSV(req.responseText);
-  };
+const outputElement = document.getElementById('output_csv');
+
+function getCsvData(dataPath) {
+ const request = new XMLHttpRequest();
+ request.addEventListener('load', (event) => {
+  const response = event.target.responseText;
+  outputElement.innerHTML = response;
+ });
+ request.open('GET', dataPath, true);
+ request.send();
 }
-function setCSV(str) {
-  var data = [];
-  var dataArr;
-  var r = document.getElementById('r');
-  var tmp = str.split('\n');
-  tmp.forEach(x => {
-    dataArr = x.split(',');
-    if (dataArr[0]) {
-      data.push(dataArr.map(x => x.trim()));
-    }
-  });
-  var t = '<dl>';
-  data.forEach(x => {
-    t += `<dt>${x[0]}</dt><dd>価格は${x[1]}円</dd>`;
-  });
-  t += '</dl>';
-  r.innerHTML = t;
-}
-getCSV();
+
+getCsvData('./example.csv');
